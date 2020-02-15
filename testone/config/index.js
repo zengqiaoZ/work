4577166b9fceb694c69ec61b1a0ea2f2360a9ce1
zengqiaoZ <2911907898@qq.com>
2020-02-15 12:06:56 +0800

    testone3

--- testone/config/index.js
+++ testone/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/work/dist',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
